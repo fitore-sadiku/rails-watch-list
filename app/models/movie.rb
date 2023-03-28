1ca3cc5bef6875cb35b37872793b2efae0f5b9fb
Fitore Sadiku <fitore.sadiku@hotmail.com>
2023-03-28 21:17:36 +1100

    models coded

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :poster_url, presence: true
+  validates :rating, presence: true, inclusion: { in: 0..10 }
+
+  has_many :bookmarks
 end
